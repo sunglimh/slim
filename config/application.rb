7afb7656f8c5e73a58504627057e48f6a58bc65f
Sung <sunglimh@gmail.com>
2013-09-19 16:07:07 -0700

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,8 @@ Bundler.require(:default, Rails.env)
 
 module Slim
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+	# Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
