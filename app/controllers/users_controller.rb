05d7c10d9f878ad329b62f69e8a47c6fd7d37ae5
Sung <sunglimh@gmail.com>
2013-09-26 17:33:19 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,10 @@ class UsersController < ApplicationController
 	end
 	
 	def create
-		@user = User.new(user_params)    
+		@user = User.new(user_params)
 		if @user.save
-			flash[:success] = "Welcome to Slim!"
+			sign_in @user
+			flash[:success] = "Welcome to the Slim!"
 			redirect_to @user
 		else
 			render 'new'
